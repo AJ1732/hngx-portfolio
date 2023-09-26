28b81fac7feb4c3338342fd004f2d3f29cfca371
AJ1732 <ejemeniboi@gmail.com>
2023-09-26 15:43:39 +0100

    Fixed White Spaces

--- src/components/Pages/AboutMe.jsx
+++ src/components/Pages/AboutMe.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AboutMe = () => {
   return (
-    <div id='aboutme' className='min-h-screen w-full h-full py-10 px-10 |  | flex flex-col gap-10'>
+    <div id='aboutme' className='min-h-fit w-full h-full py-10 px-10 |  | flex flex-col gap-10'>
       <h2 className='font-light text-4xl'>About Me</h2>
       <div className='font-medium text-xl flex flex-col gap-2'>
         <p>An inquisitive and motivated Frontend Engineer, willing to obtain such position to utilize my skills in web development and programming, while continuously improving my skills and knowledge in this field.</p>
@@ -10,7 +10,7 @@ const AboutMe = () => {
         <p>Fluent in English, beginner in French and Korean</p>
       </div>
 
-      <div className='min-h-screen flex flex-col  justify-center items-center gap-3'>
+      <div className='min-h-fit flex flex-col  justify-center items-center gap-3'>
         <h2 className='font-light text-2xl self-start'>Skills</h2>
         <ul className='w-full flex flex-wrap justify-between items-center gap-4 text-white'>
           <li className=' bg-light-brown py-2 px-5 rounded'>HTML</li>
--- src/components/Pages/Home.jsx
+++ src/components/Pages/Home.jsx
@@ -4,7 +4,7 @@ import portfolioImage from '../../assets/portfolioImage.jpeg'
 
 const Home = () => {
   return (
-    <div id='home' className='min-h-calc py-24 px-10 flex flex-col justify-center items-center gap-4 | md:h-screen md:py-10 sm:px-20 md:flex-row  md:gap-40'>
+    <div id='home' className='min-h-calc py-24 px-10 flex flex-col justify-center items-center gap-4 | md:h-screen md:py-10 sm:px-20 md:flex-row md:justify-center md:items-center md:gap-40'>
       <img 
         src={portfolioImage} 
         className='w-64 h-64 object-cover rounded-full | md:w-96 md:h-96'
